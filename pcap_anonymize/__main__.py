adfb92c23f7548833c0a895726b9b722f28861c3
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2024-12-19 15:09:55 +0100

    Python package meta config

--- pcap_anonymize/__main__.py
+++ pcap_anonymize/__main__.py
@@ -3,13 +3,16 @@ import argparse
 from .pcap_anonymize import anonymize_pcap
 
 
+### MAIN FUNCTION ###
 def main() -> None:
-    """
-    Main function for the CLI.
-    """
     parser = argparse.ArgumentParser(description="Anonymize a PCAP traffic capture.")
-    parser.add_argument("input", type=os.PathLike, help="Path to the input PCAP file.")
-    parser.add_argument("-o", "--output", type=os.PathLike, help="Path to the output PCAP file.")
+    parser.add_argument("input", type=str, help="Path to the input PCAP file.")
+    parser.add_argument("-o", "--output", type=str, help="Path to the output PCAP file.")
     args = parser.parse_args()
 
     anonymize_pcap(args.input, args.output)
+
+
+### ENTRY POINT ###
+if __name__ == "__main__":
+    main()
